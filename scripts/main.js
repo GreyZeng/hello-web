4be7d07ba7401cf778d0fc248f6e9fcac620571e
GreyZeng <410486047@qq.com>
2022-07-03 08:59:44 +0800

    init project

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+}
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox.jpg') {
+      myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+      myImage.setAttribute('src', 'images/firefox.jpg');
+    }
+}
+
+myButton.onclick = function(){
+	setUserName();
+}
